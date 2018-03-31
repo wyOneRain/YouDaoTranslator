a56be01667ab0347927bd1e065c138339c0dc98b
wyOneRain <1015933411@qq.com>
2018-03-31 22:57:30 +0800

    Add files via upload

---
+++ Translator.py
@@ -0,0 +1,36 @@
+import config
+import hashlib
+import time
+import requests
+
+class Translator:
+    def __init__(self):
+        self.API_ul = "http://openapi.youdao.com/api"
+        self.appKey = config.appKey
+        self.appSercret = config.secretKey
+        self.Trans_from = config.trans_from
+        self.Trans_to = config.trans_to
+
+    def getJsonData(self,queryText):
+        '''
+        将数据url编码
+        :param queryText: 待翻译的文字
+        :return: 返回url编码过的数据
+        '''
+        salt = str(int(round(time.time() * 1000))) #产生产生随机数 ,其实固定值也可以,比如"2"
+        sign_text = self.appKey + queryText + salt +self.appSercret
+        sign = hashlib.md5(sign_text.encode('utf-8')).hexdigest()
+
+        payload = {
+            'q': queryText,
+            'from': self.Trans_from,
+            'to': self.Trans_to,
+            'appKey': self.appKey,
+            'salt': salt,
+            'sign': sign
+        }
+        data = requests.get(self.API_ul,payload);
+
+        data = data.json()['translation']
+        return data[0]
+
---
+++ config.py
@@ -0,0 +1,6 @@
+#utf-8
+
+appKey = '030038af76185889'
+secretKey = 'dwa16JzjeY3ME4jfVYWtqCYrlU90nspQ'
+trans_to = "zh-CHS"
+trans_from = 'EN'
